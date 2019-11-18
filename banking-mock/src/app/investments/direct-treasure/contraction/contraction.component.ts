a9426ee6348b3727ab312d1727f2ad426fce13d8
Bruno Cesar Aparecido Gaudino <brunogaudino@brq.com>
2019-11-18 15:05:58 -0300

    Adjust in the public title flux for the future implementation

--- banking-mock/src/app/investments/direct-treasure/contraction/contraction.component.ts
+++ banking-mock/src/app/investments/direct-treasure/contraction/contraction.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import * as $ from 'jquery';
 
 import { ReduxService } from 'src/app/shared/services/redux.service';
-import { Router } from '@angular/router';
 import { DataShareService } from 'src/app/shared/services/data-share.service';
 
 @Component({
@@ -13,21 +14,37 @@ export class ContractionComponent implements OnInit {
 
   objPublicTitles: Object = {};
   objDataShare: object;
+  @ViewChild('valueToApply', {static: false}) valueToApply : ElementRef;
 
   constructor(
     private reduxService: ReduxService,
     private router: Router,
-    private dataShareService: DataShareService
+    private dataShareService: DataShareService,
+    private nativeElement: ElementRef
   ) { }
 
   ngOnInit() {
     //this.objPublicTitles = this.reduxService.getRedux('publicTitleReducer');
     this.objPublicTitles = this.dataShareService.getObjDataShare();
-    //console.log('Contration do service sahred ', this.objPublicTitles);
   }
 
   clickContractTitle(titleCode: number){
-    this.router.navigate(['/investments/direct-treasure/confirmation', titleCode]);
+    console.log('Click contract title ', this.valueToApply.nativeElement.value);
+    //this.router.navigate(['/investments/direct-treasure/confirmation', titleCode]);
   }
 
+  focusOut(){
+    console.log('Call simulation and validation service');
+    console.log('FOCUS OUT ', this.valueToApply.nativeElement.value);
+  }
 }
+
+/*
+Needs implement the application simulation in the input value apply
+
+------Add-----
+yields - create a json list
+value - input in app
+minimal value - input in app
+repeatly month - input in app
+*/
